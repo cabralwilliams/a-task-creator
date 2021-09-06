cbfdbb2e3a6e3a15fbd9797cdff93b2c416615f4
Cabral Williams <cabral.williams@gmail.com>
2021-09-06 15:46:06 -0500

    adjustments made to form - li item data gathered in div element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
 
 var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 //var buttonEl = document.querySelector("#save-task");
 
 var createTaskHandler = function(event) {
     //console.log(event);
     event.preventDefault();
-    var newElement = document.createElement("li");
-    newElement.className = "task-item";
-    newElement.textContent = "A new task was added!";
-    document.querySelector("#tasks-to-do").appendChild(newElement);
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeValue = document.querySelector("select[name='task-type']").value;
+    var liToAdd = document.createElement("li");
+    liToAdd.className = "task-item";
+    var newElement = document.createElement("div");
+    newElement.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeValue +"</span>";
+    liToAdd.appendChild(newElement)
+    //newElement.textContent = taskNameInput;
+    tasksToDoEl.appendChild(liToAdd);
+    console.dir(liToAdd);
 }
 
 formEl.addEventListener("submit",createTaskHandler);
