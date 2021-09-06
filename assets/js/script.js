7d65478288837d49c89e4ab8a7c62eb3fc198d03
Cabral Williams <cabral.williams@gmail.com>
2021-09-06 14:06:44 -0500

    form updated and submit function properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,14 @@
 
-//console.dir(window.document);
-//var btn = window.document.querySelector("button");
-//console.log(btn)
-//console.dir(btn);
-//console.log(document.querySelector("button").textContent);
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
+//var buttonEl = document.querySelector("#save-task");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    //console.log(event);
+    event.preventDefault();
     var newElement = document.createElement("li");
     newElement.className = "task-item";
     newElement.textContent = "A new task was added!";
     document.querySelector("#tasks-to-do").appendChild(newElement);
 }
 
-//buttonEl.addEventListener("click", function() {
-    //alert("Button Clicked!");
-    //document.querySelector(".task-list").append(document.createElement("li"));
-    //The above can also be accomplished by creating element, adding class to the element via .className, adding the textContent, and then using .appendChild
-    //document.querySelector(".task-list").lastChild.classList.add("task-item");
-    //document.querySelector(".task-list").lastChild.textContent = "A new task was added!";
-//});
-
-buttonEl.addEventListener("click",createTaskHandler);
+formEl.addEventListener("submit",createTaskHandler);
